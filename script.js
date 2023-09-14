d26d82e670ec958f576ad320e237f07c3f818cfc
Gabr1elGom3s <santos19e15@gmail.com>
2023-09-14 10:16:05 -0300

    commit part 2

--- script.js
+++ script.js
@@ -20,20 +20,15 @@ const saveTodo = (text) => {
     todoTitle.innerText = text;
     todo.appendChild(todoTitle);
 
- 
-
     const editBtn = document.createElement('button');
     editBtn.classList.add('editTodo');
     editBtn.innerHTML = '<i class="fa-solid fa-pen"></i>';
     todo.appendChild(editBtn);
 
- 
-
     const removeBtn = document.createElement('button');
     removeBtn.classList.add('removeTodo');
     removeBtn.innerHTML = '<i class="fa-solid fa-trash"></i>';
     todo.appendChild(removeBtn);
-
 
     const doneBtn = document.createElement('button');
     doneBtn.classList.add('finishTodo');
@@ -42,16 +37,67 @@ const saveTodo = (text) => {
 
     todoList.appendChild(todo);
 
-
     todoInput.value = ''
     todoInput.focus();
+    //console.log(todo)
 
 }
 
+// editar to do:
+const toggleForms = () =>{
+  editForm.classList.toggle('hide');
+  todoForm.classList.toggle('hide');
+  todoList.classList.toggle('hide');
+
+
+}
+
+// Eventos
+
+// 01 - adicionar tarefas:
 todoForm.addEventListener('submit', (e) => {
     e.preventDefault();
+    //conseole.log("enviou uma form")
     const inputValue = todoInput.value;
+
     if (inputValue) {
+      //conseole.log("inputValue")
         saveTodo(inputValue);
     }
+});
+
+//04 - Atribuindo função aos botões:
+
+document.addEventListener('click',  (e) =>{
+  const targetEl = e.target;
+  const parentEl = targetEl.closest('div');
+
+  if(targetEl.classList.contains('finishTodo')){
+    //console.log("clicou pra finalizar")
+    parentEl.classList.toggle('done');
+
+  }
+
+  //remover tarefa
+  if(targetEl.classList.contains('removeTodo')){
+    parentEl.remove();
+  }
+
+  //editar tarefa
+  let todoTitle;
+  if(parentEl && parentEl.querySelector('h3')){
+    todoTitle = parentEl.querySelector('h3').innerText;
+
+  }
+  
+  
+  
+  if(targetEl.classList.contains('editTodo')){
+    toggleForms();
+
+    editInput.value = todoTitle
+    oldInputValue = todoTitle
+  }
+  
+
 });
